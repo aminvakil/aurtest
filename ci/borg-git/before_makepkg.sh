bb458c8652878ef9135fed842e7689837c7a0556
Amin Vakil <info@aminvakil.com>
2022-04-25 21:19:17 +0430

    Fix shellcheck

--- ci/borg-git/before_makepkg.sh
+++ ci/borg-git/before_makepkg.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 set -eux
 pacman -Syu git --noconfirm
-for i in {libdeflate-git}; do
-        git clone "https://aur.archlinux.org/$i.git"
-        chown -R devel: "$i"
-        su devel sh -c "cd $i && makepkg -sri --noconfirm"
-done;
+i="libdeflate-git"
+git clone "https://aur.archlinux.org/$i.git"
+chown -R devel: "$i"
+su devel sh -c "cd $i && makepkg -sri --noconfirm"
